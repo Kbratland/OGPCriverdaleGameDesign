0dbf16acc59bbed810b7e2e9a48494592c6523bc
akaganov123 <119634271+akaganov123@users.noreply.github.com>
2023-01-05 13:54:37 -0800

    1/5/2023
    Inventory drag drop prototype done

--- GameDesign2_Game2/Assets/InventoryItem.cs
+++ GameDesign2_Game2/Assets/InventoryItem.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
 {
+    public Image image;
     [HideInInspector] public Transform parentAfterDrag;
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("Begin drag");
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
+        image.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData) {
@@ -22,5 +25,6 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("End drag");
         transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
     }
 }
