b15b4d119131b70b46bbe341207b439ff04f16ed
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-01-13 14:34:40 -0800

    1/13

--- GameDesign2_Game2/Assets/Scripts/Inventory scripts/InvenDisplay.cs
+++ GameDesign2_Game2/Assets/Scripts/Inventory scripts/InvenDisplay.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class InvenDisplay : MonoBehaviour
 {
+    public int index;
+    public InvenData invenData;
+    Image image;
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        image = invenData.invenItems[index];
     }
 }
