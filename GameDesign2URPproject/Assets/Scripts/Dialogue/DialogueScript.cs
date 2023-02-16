77a4e2ebd31345952ea436a72549b840b29557e5
akaganov123 <119634271+akaganov123@users.noreply.github.com>
2023-02-16 13:28:23 -0800

    2/16

---
+++ GameDesign2URPproject/Assets/Scripts/Dialogue/DialogueScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+  enum GameStates {npc1, npc2};
+  public TMP_Text txtTitle;
+  public TMP_Text txtDialoguePlace;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//      switch (currState){
+//        case GameStates.npc1
+//           InDialogue();
+//           break; 
+//      }
+//       }
+      
+// void inDialogue(){
+//       txtContent.text = ".";
+//         // if (Input.GetKeyDown(KeyCode.UpArrow)){
+//         //   currState = GameStates.npc2;
+//         }
+}
+
+
+
+
